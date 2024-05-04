744a276c509d45c22c83b1244a01176357406098
sadaf <145974455+sdaz9087@users.noreply.github.com>
2024-05-05 01:54:03 +0330

    index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 // import './index.css';
-import Weather from './Weather';
-import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import Weather from "./Weather";
+import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.min.css";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
-    
-    <Weather defaultcity = "Kenya"/>
-    
+
+  <Weather defaultcity="Tehran" />
+
   // </React.StrictMode>
 );
-
 
 reportWebVitals();
